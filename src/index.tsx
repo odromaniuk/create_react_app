d9105d51982c8b18538559fb08f4eecd2508c3ab
zuser <romanyuk.ad@gmail.com>
2017-08-08 02:12:22 +0300

    added react example

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+/**
+ * Created by zuser on 4/20/17.
+ */
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+import {Hello} from "./components/Hello"
+
+ReactDOM.render(<Hello compiler="typescript fa" framework="React fa"/>, document.getElementById("example"))
